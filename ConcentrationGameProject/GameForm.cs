494f79180f6b01d59017a3144e206b92ee35559c
YiannosHg <48808892+YiannosHg@users.noreply.github.com>
2021-11-21 17:44:19 +0200

    Created check for match function

--- ConcentrationGameProject/GameForm.cs
+++ ConcentrationGameProject/GameForm.cs
@@ -19,7 +19,8 @@ namespace ConcentrationGameProject
         int rule = 2;
         int cardSize = 60;
         int countMoves = 0;
-        int clickedPictures = 0;
+        List<Button> moves = new List<Button>();
+        int matchedPictures = 0;
 
         public GameForm()
         {
@@ -29,6 +30,10 @@ namespace ConcentrationGameProject
         private void GameForm_Load(object sender, EventArgs e)
         {
             createPictureButtons();
+            //gameLoop();
+            /*while (moves.Count != rule)
+                if (0 == countMoves % rule)
+                    checkForMatch();*/
         }
 
         private void createPictureButtons()
@@ -43,18 +48,9 @@ namespace ConcentrationGameProject
                     pictureButtons[i + j * size] = new Button(); // Add button in array
                     pictureButtons[i + j * size].Width = cardSize;
                     pictureButtons[i + j * size].Height = cardSize;
-
                     pictureButtons[i + j * size].BackgroundImage = Properties.Resources.qMark;
                     pictureButtons[i + j * size].BackgroundImageLayout = ImageLayout.Zoom;
-
-                    //string imageName = "image" + i;
-                    //pictureButtons[i+j*size].Image = (Bitmap)Properties.Resources.ResourceManager.GetObject(imageName);
-                    //pictureButtons[i + j * size].Tag = i;
-
-                    string imageName = "image" + i;
-                    //pictureButtons[i + j * size].Tag = (Bitmap)Properties.Resources.ResourceManager.GetObject(imageName);
-                    pictureButtons[i + j * size].Tag = imageName;
-
+                    pictureButtons[i + j * size].Tag = i;
                     pictureButtons[i + j * size].Location = new Point(cardSize * (i % size) + cardSize + i * 10, (cardSize * (i / size) + cardSize + 2 * j * cardSize) + 10);
 
                     // Events
@@ -66,6 +62,11 @@ namespace ConcentrationGameProject
 
         private void pictureButtons_ClickHandler(object sender, System.EventArgs e)
         {
+            string imageName = "image" + ((Button)sender).Tag;
+            ((Button)sender).BackgroundImage = (Bitmap)Properties.Resources.ResourceManager.GetObject(imageName);
+            moves.Add((Button)sender);
+            ++countMoves;
+
             // Bitmap tempPic = (Bitmap)Properties.Resources.ResourceManager.GetObject((string)((Button)sender).Tag);
             /*Bitmap tempPic = (Bitmap)((Button)sender).BackgroundImage;
             ((Button)sender).BackgroundImage = (Bitmap)Properties.Resources.ResourceManager.GetObject((string)((Button)sender).Tag);
@@ -73,6 +74,52 @@ namespace ConcentrationGameProject
 
             /*string imageName = "image" + ((Button)sender).Tag;
             ((Button)sender).BackgroundImage = (Bitmap)Properties.Resources.ResourceManager.GetObject(imageName);*/
+        }
+
+        private void gameLoop()
+        {
+            while (matchedPictures < size)
+                if (0 == countMoves % rule)
+                    checkForMatch();
+
+            MessageBox.Show($"Total clicks: {countMoves}");
+        }
+
+        private void checkForMatch()
+        {
+            //List<int> movesToCheck = new List<int>();
+
+            if (2 == rule)
+            {
+                if (moves[0].Tag == moves[1].Tag)
+                {
+                    moves[0].Enabled = false;
+                    moves[1].Enabled = false;
+                    ++matchedPictures;
+                }
+                else
+                {
+                    moves[0].BackgroundImage = Properties.Resources.qMark;
+                    moves[1].BackgroundImage = Properties.Resources.qMark;
+                }
+            }
+            else // 3 = rule
+            {
+                if (moves[0].Tag == moves[1].Tag && moves[0].Tag == moves[2].Tag)
+                {
+                    moves[0].Enabled = false;
+                    moves[1].Enabled = false;
+                    moves[2].Enabled = false;
+                    ++matchedPictures;
+                }
+                else 
+                {
+                    moves[0].BackgroundImage = Properties.Resources.qMark;
+                    moves[1].BackgroundImage = Properties.Resources.qMark;
+                    moves[2].BackgroundImage = Properties.Resources.qMark;
+                }
+            }
+            moves.Clear();
         }
     }
 }
